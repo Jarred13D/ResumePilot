2987c4b57a981131ac4ef4e726f804236e5606c1
mcclin53 <dan.mcclintic@gmail.com>
2025-03-25 20:50:45 -0400

    added resume-routes file

---
+++ resumepilot/server/src/routes/api/resume-routes.ts
@@ -0,0 +1,81 @@
+import express from 'express';
+import type { Request, Response } from 'express';
+import { ResumeData } from '../../models/resumeData.ts';
+
+const router = express.Router();
+
+
+// GET /resume - Get all resume info
+router.get('/', async (_req: Request, res: Response) => {
+    try {
+      const resumeInfo = await ResumeData.findAll({
+        attributes: { exclude: ['password'] }
+      });
+      res.json(resumeInfo);
+    } catch (error: any) {
+      res.status(500).json({ message: error.message });
+    }
+  });
+
+// GET /resume/:id - Get resume info by id
+router.get('/:id', async (req: Request, res: Response) => {
+    const { id } = req.params;
+    try {
+      const resumeInfo = await ResumeData.findByPk(id);
+      if (resumeInfo) {
+        res.json(resumeInfo);
+      } else {
+        res.status(404).json({ message: 'Resume info not found' });
+      }
+    } catch (error: any) {
+      res.status(500).json({ message: error.message });
+    }
+  });
+
+// POST /resume - Create a new resume
+router.post('/', async (req: Request, res: Response) => {
+    const { } = req.body;
+    try {
+      const newResume = await ResumeData.create({ //.add info here });
+      res.status(201).json(newResume);
+    } catch (error: any) {
+      res.status(400).json({ message: error.message });
+    }
+  });
+
+  // PUT /resume/:id - Update a user by id
+router.put('/:id', async (req: Request, res: Response) => {
+    const { id } = req.params;
+    const {//add info here// } = req.body;
+    try {
+      const resumeInfo = await ResumeData.findByPk(id);
+      if (resumeInfo) {
+        resumeInfo.//add info here = add info here;
+        resumeInfo.//add info here = add info here;
+        await resumeInfo.save();
+        res.json(resumeInfo);
+      } else {
+        res.status(404).json({ message: 'Resume info not found' });
+      }
+    } catch (error: any) {
+      res.status(400).json({ message: error.message });
+    }
+  });
+
+// DELETE /resume/:id - Delete resume info by id
+router.delete('/:id', async (req: Request, res: Response) => {
+    const { id } = req.params;
+    try {
+      const user = await ResumeData.findByPk(id);
+      if (resumeInfo) {
+        await resumeInfo.destroy();
+        res.json({ message: 'Resume info deleted' });
+      } else {
+        res.status(404).json({ message: 'Resume info not found' });
+      }
+    } catch (error: any) {
+      res.status(500).json({ message: error.message });
+    }
+  });
+  
+  export { router as resumeRouter };
