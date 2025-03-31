1b6d36de52a4542cf87a739e4a7523811f95e6d5
Matt Mullen <mhmullen@icloud.com>
2025-03-30 17:24:47 -0700

    updated gitignore

--- resumepilot/server/src/server.ts
+++ resumepilot/server/src/server.ts
@@ -1,51 +1,53 @@
-// const forceDatabaseRefresh = false;
-
-// import dotenv from 'dotenv';
-// dotenv.config();
-
-// import express from 'express';
-// import sequelize from './config/connection.js';
-// import routes from './routes/index.js';
-
-// const app = express();
-// const PORT = process.env.PORT || 3001;
-
-// // Serves static files in the entire client's dist folder
-// app.use(express.static('../client/dist'));
-
-// app.use(express.json());
-// app.use(routes);
-
-// sequelize.sync({force: forceDatabaseRefresh}).then(() => {
-//   app.listen(PORT, () => {
-//     console.log(`Server is listening on port ${PORT}`);
-//   });
-// });
 const forceDatabaseRefresh = false;
 
-import express from "express";
-import sequelize from "./config/connection.js";
-import routes from "./routes/index.js";
-import cors from "cors"; // allows cross-origin resource sharing, allowing the client to make requests to the server from a different domain (may not need this)
-import dotenv from "dotenv";
-import aiRoutes from "./routes/aiRoutes.js";
-
+import dotenv from 'dotenv';
 dotenv.config();
 
-const app = express();
-app.use(cors()); // Enable CORS
-app.use("/api/ai", aiRoutes); // Use the AI routes
+import express from 'express';
+import sequelize from './config/connection.js';
+import routes from './routes/index.js';
 
+const app = express();
 const PORT = process.env.PORT || 3001;
 
 // Serves static files in the entire client's dist folder
-app.use(express.static("../client/dist"));
+app.use(express.static('../client/dist'));
 
 app.use(express.json());
 app.use(routes);
 
-sequelize.sync({ force: forceDatabaseRefresh }).then(() => {
+sequelize.sync({force: forceDatabaseRefresh}).then(() => {
   app.listen(PORT, () => {
     console.log(`Server is listening on port ${PORT}`);
   });
 });
+
+
+// const forceDatabaseRefresh = false;
+
+// import express from "express";
+// import sequelize from "./config/connection.js";
+// import routes from "./routes/index.js";
+// import cors from "cors"; // allows cross-origin resource sharing, allowing the client to make requests to the server from a different domain (may not need this)
+// import dotenv from "dotenv";
+// import aiRoutes from "./routes/aiRoutes.js";
+
+// dotenv.config();
+
+// const app = express();
+// app.use(cors()); // Enable CORS
+// app.use("/api/ai", aiRoutes); // Use the AI routes
+
+// const PORT = process.env.PORT || 3001;
+
+// // Serves static files in the entire client's dist folder
+// app.use(express.static("../client/dist"));
+
+// app.use(express.json());
+// app.use(routes);
+
+// sequelize.sync({ force: forceDatabaseRefresh }).then(() => {
+//   app.listen(PORT, () => {
+//     console.log(`Server is listening on port ${PORT}`);
+//   });
+// });
